e8ed368ab6fe5995c4b08cb77dba6c6dfa49ee34
Camilo Romero <camilo.romero@payu.com>
2021-08-17 23:15:54 -0500

    Include .DS_Store on nvimtree ignore

--- config.lua
+++ config.lua
@@ -46,7 +46,7 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.show_icons.git = 0
 lvim.builtin.nvimtree.hide_dotfiles = 0
-lvim.builtin.nvimtree.ignore = { ".git", ".cache" }
+lvim.builtin.nvimtree.ignore = { ".git", ".cache", ".DS_Store" }
 -- lvim.builtin.dap.active = true
 
 -- Treesitter
