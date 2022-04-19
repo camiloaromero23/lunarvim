77d76256a940a585301c61d3adb5f74372dc5c61
Camilo Romero <camiloaromero23@gmail.com>
2022-04-18 22:48:56 -0500

    Update config colors & disable extended mode

--- lua/user/ts-rainbow.lua
+++ lua/user/ts-rainbow.lua
@@ -6,17 +6,24 @@ M.config = function()
     return
   end
 
+  -- vim.cmd [[hi rainbowcol1 guifg=Gold]]
+  -- vim.cmd [[hi rainbowcol2 guifg=Orchid]]
+  -- vim.cmd [[hi rainbowcol3 guifg=LightSkyBlue]]
   rainbow.setup {
     rainbow = {
       enable = true,
-      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+      extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
       max_file_lines = 5000, -- Do not enable for files with more than n lines, int
       colors = {
         "Gold",
         "Orchid",
         "LightSkyBlue",
       }, -- table of hex strings
-      -- termcolors = {} -- table of colour name strings
+      -- termcolors = {
+      --   "rainbowcol1",
+      --   "rainbowcol2",
+      --   "rainbowcol3",
+      -- }, -- table of colour name strings
     },
   }
 end
