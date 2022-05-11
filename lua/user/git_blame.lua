9220761e355a7f506e10d85f8f345a9ee191c75a
Camilo Romero <camiloaromero23@gmail.com>
2022-05-11 14:40:11 -0500

    Remove NvimTree git blame

--- lua/user/git_blame.lua
+++ lua/user/git_blame.lua
@@ -3,6 +3,7 @@ local M = {}
 M.config = function()
   vim.g.gitblame_message_template = "      <author>, <date> • <summary>"
   vim.g.gitblame_date_format = "%r"
+  vim.g.gitblame_ignored_filetypes = { "NvimTree" }
 end
 
 return M
