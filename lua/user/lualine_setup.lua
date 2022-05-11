7cd06d0b442b4dd946cec11affb58bdad0bbf30f
Camilo Romero <camiloaromero23@gmail.com>
2022-05-11 14:39:21 -0500

    Include custom branch icon alternative

--- lua/user/lualine_setup.lua
+++ lua/user/lualine_setup.lua
@@ -6,6 +6,7 @@ M.config = function()
     local config = lualine.get_config()
     local components = require "lvim.core.lualine.components"
     local colors = require "lvim.core.lualine.colors"
+    -- components.branch.icon = " "
 
     config.sections.lualine_a = { components.mode }
     config.sections.lualine_b = {}
