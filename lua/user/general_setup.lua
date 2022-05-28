659d27afd7b66bbaff4ec539d1ebce8c6c7c835e
Camilo Romero <camiloaromero23@gmail.com>
2022-05-28 11:41:50 -0500

    Disable foldenable at start

--- lua/user/general_setup.lua
+++ lua/user/general_setup.lua
@@ -3,5 +3,6 @@ lvim.lint_on_save = true
 vim.opt.hlsearch = false
 vim.opt.cmdheight = 1
 vim.opt.relativenumber = true
+vim.opt.foldenable = false
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
