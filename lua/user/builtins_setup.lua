4f4445a690514aa5eb7c08d0b9593bb7b9217fd6
Camilo Romero <camiloaromero23@gmail.com>
2022-05-28 19:14:00 -0500

    Fix "git" named files not showing on NvimTree

--- lua/user/builtins_setup.lua
+++ lua/user/builtins_setup.lua
@@ -2,5 +2,5 @@ lvim.builtin.alpha.active = true
 lvim.builtin.bufferline.options.always_show_bufferline = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.show_icons.git = 0
-lvim.builtin.nvimtree.setup.filters.custom = { ".git", ".cache", ".DS_Store" }
+lvim.builtin.nvimtree.setup.filters.custom = { ".cache", ".DS_Store" }
 -- lvim.builtin.dap.active = true
