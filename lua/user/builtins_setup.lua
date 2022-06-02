15ec264fff5680ad9c7cba44af770298be21ee0b
Camilo Romero <camilo.romero@kushkipagos.com>
2022-06-01 23:29:28 -0500

    Update ignore files

--- lua/user/builtins_setup.lua
+++ lua/user/builtins_setup.lua
@@ -1,6 +1,7 @@
 lvim.builtin.alpha.active = true
 lvim.builtin.bufferline.options.always_show_bufferline = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.filters.custom = { ".cache", ".DS_Store" }
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.filters.custom = { "\\.git", "\\.cache", "\\.DS_Store" }
+lvim.builtin.nvimtree.setup.filters.exclude = { ".gitignore" }
 -- lvim.builtin.dap.active = true
