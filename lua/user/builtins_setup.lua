12fece9537784a397afceb845789de65170e2da6
Camilo Romero <camilo.romero@kushkipagos.com>
2022-06-01 23:29:13 -0500

    Fix icons config

--- lua/user/builtins_setup.lua
+++ lua/user/builtins_setup.lua
@@ -1,6 +1,6 @@
 lvim.builtin.alpha.active = true
 lvim.builtin.bufferline.options.always_show_bufferline = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.show_icons.git = 0
 lvim.builtin.nvimtree.setup.filters.custom = { ".cache", ".DS_Store" }
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 -- lvim.builtin.dap.active = true
