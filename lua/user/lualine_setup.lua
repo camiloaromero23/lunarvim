2a429ec3fca5e4c90db37cb1e71b42515dca971b
Camilo Romero <camilo.romero@kushkipagos.com>
2022-06-06 21:53:00 -0500

    Lualine re-setup

--- lua/user/lualine_setup.lua
+++ lua/user/lualine_setup.lua
@@ -1,6 +1,7 @@
 local M = {}
+
 M.config = function()
-  lvim.builtin.lualine.options.theme = "material-nvim"
+  -- lvim.builtin.lualine.options.theme = "material-nvim"
 
   lvim.builtin.lualine.on_config_done = function(lualine)
     local config = lualine.get_config()
@@ -10,9 +11,8 @@ M.config = function()
 
     config.sections.lualine_a = { components.mode }
     config.sections.lualine_b = {}
-    config.sections.lualine_c = { components.branch, components.filename, components.diff }
+    config.sections.lualine_c = { components.branch, components.filename, components.diff, components.diagnostics }
     config.sections.lualine_x = {
-      components.diagnostics,
       components.filetype,
       components.lsp,
       components.encoding,
