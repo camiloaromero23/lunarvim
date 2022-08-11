34e00442b753899e45fc2cd785239b9a23a78310
Camilo Romero <camilo.romero@kushkipagos.com>
2022-08-11 15:41:58 -0500

    Update custom icon colors

--- lua/user/devicons.lua
+++ lua/user/devicons.lua
@@ -1,42 +1,56 @@
 local devicons = require "nvim-web-devicons"
+
+local colors = {
+  angularRed = "#dd1b16",
+  cssBlue = "#029ae6",
+  gitignoreRed = "#fc3404",
+  htmlRed = "#e34c26",
+  jsYellow = "#f7df1e",
+  packageJsonGreen = "#75bb39",
+  testTeal = "#04bbcb",
+  tsBlue = "#3178c6",
+  txtBlue = "#05a8fa",
+  vueGreen = "#42B883",
+}
+
 local custom_icons = {
   css = {
     icon = "",
-    color = "#029ae6",
+    color = colors.cssBlue,
     name = "css",
   },
   [".env"] = {
     icon = "",
-    color = "#f7df1e",
+    color = colors.jsYellow,
     name = "env",
   },
   html = {
     icon = "",
-    color = "#e34c26",
+    color = colors.htmlRed,
     cterm_color = "166",
     name = "Html",
   },
   js = {
     icon = "",
-    color = "#f7df1e",
+    color = colors.jsYellow,
     cterm_color = "185",
     name = "js",
   },
   ts = {
     icon = "ﯤ",
-    color = "#3178c6",
+    color = colors.tsBlue,
     cterm_color = "67",
     name = "ts",
   },
   tsx = {
     icon = "",
-    color = "#3178c6",
+    color = colors.tsBlue,
     cterm_color = "67",
     name = "tsx",
   },
   txt = {
     icon = "",
-    color = "#05a8fa",
+    color = colors.txtBlue,
     cterm_color = "67",
     name = "txt",
   },
@@ -44,82 +58,70 @@ local custom_icons = {
   vue = {
     icon = "﵂",
     cterm_color = "67",
-    color = "#42B883",
+    color = colors.vueGreen,
     name = "vue",
   },
   ["component.ts"] = {
     icon = "",
-    color = "#3178c6",
+    color = colors.tsBlue,
     name = "componentts",
   },
   ["test.js"] = {
     icon = "",
-    color = "#f7df1e",
+    color = colors.jsYellow,
     name = "testJS",
   },
   ["test.ts"] = {
     icon = "",
-    color = "#3178c6",
+    color = colors.tsBlue,
     name = "testTS",
   },
   ["test.jsx"] = {
     icon = "",
-    color = "#04bbcb",
+    color = colors.testTeal,
     name = "testJSX",
-  },
-  ["test.tsx"] = {
-    icon = "",
-    color = "#04bbcb",
-    name = "testTSX",
   },
   ["spec.js"] = {
     icon = "",
-    color = "#f7df1e",
+    color = colors.jsYellow,
     name = "testJS",
   },
   ["spec.ts"] = {
     icon = "",
-    color = "#3178c6",
+    color = colors.tsBlue,
     name = "testTS",
   },
   ["spec.jsx"] = {
     icon = "",
-    color = "#04bbcb",
+    color = colors.testTeal,
     name = "testJSX",
-  },
-  ["spec.tsx"] = {
-    icon = "",
-    color = "#04bbcb",
-    name = "testTSX",
-  },
-  ["module.ts"] = {
-    icon = "",
-    color = "#dd1b16",
-    name = "angularjson",
   },
   ["angular.json"] = {
     icon = "",
-    color = "#dd1b16",
+    color = colors.angularRed,
     name = "angularjson",
   },
   ["package.json"] = {
     icon = "",
-    color = "#75bb39",
+    color = colors.packageJsonGreen,
     name = "PackageJson",
-  },
-  ["package-lock.json"] = {
-    icon = "",
-    color = "#75bb39",
-    name = "PackageLockJson",
   },
   [".gitignore"] = {
     icon = "",
-    color = "#fc3404",
+    color = colors.gitignoreRed,
     name = "GitIgnore",
   },
 }
 
+custom_icons["component.tsx"] = custom_icons.tsx
+custom_icons["styles.tsx"] = custom_icons.tsx
+custom_icons["styles.ts"] = custom_icons.ts
+custom_icons["config.js"] = custom_icons.js
+custom_icons["module.ts"] = custom_icons["angular.json"]
+custom_icons["package-lock.json"] = custom_icons["package.json"]
+custom_icons["spec.tsx"] = custom_icons["spec.jsx"]
+custom_icons["test.tsx"] = custom_icons["test.jsx"]
+
 devicons.setup {
   override = custom_icons,
 }
-return custom_icons
