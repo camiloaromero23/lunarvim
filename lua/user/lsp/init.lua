57d33b6b7d5195728d5e8afcbbd64c84b1c8e807
Camilo Romero <camilo.romero@kushkipagos.com>
2022-08-16 16:16:19 -0500

    Update config

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,6 +1,6 @@
+require "user.lsp.config"
 require "user.lsp.bash"
 require "user.lsp.emmet"
-require "user.lsp.tailwind"
+-- require "user.lsp.tailwind"
 
 require "user.lsp.custom"
-require "user.lsp.config"
